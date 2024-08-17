0882f5a8bcb15c8ab88cbc286a8989728b442b23
Mourad <mouradmailing@gmail.com>
2024-08-17 23:20:43 +0100

    Add event fields

--- elepho-api/app/Models/Event.php
+++ elepho-api/app/Models/Event.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title',
+        'bo_title',
+        'slug',
+        'client',
+        'date',
+        'date_title',
+        'thumbnail',
+        'cover',
+        'excerpt',
+        'content',
+        'is_active',
+        'is_visible_event_fe',
+        'is_visible_event_bo',
+        'is_closed_registration',
+        'has_abstracts',
+        'abstract_limit',
+        'is_closed_abstract_submission',
+        'registration_sender_email',
+        'registration_sender_name',
+        'registration_email_subject',
+        'registration_reply_to_email',
+        'registration_cc_email',
+        'registration_bcc_email',
+        'abstract_email_subject',
+        'confirmation_email_subject',
+        'has_confirmation_mail',
+        'has_attachment_badge',
+        'attachment_badge_name',
+        'accent_color',
+        'mailer'];
 }
--- elepho-api/database/migrations/2024_08_17_213348_create_events_table.php
+++ elepho-api/database/migrations/2024_08_17_213348_create_events_table.php
@@ -13,6 +13,36 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->string('bo_title', 255)->nullable();
+            $table->string('slug', 255);
+            $table->string('client', 255)->nullable();
+            $table->date('date')->nullable();
+            $table->string('date_title', 255)->nullable();
+            $table->string('thumbnail', 500)->nullable();
+            $table->string('cover', 255)->nullable();
+            $table->string('excerpt', 2000)->nullable();
+            $table->string('content', 4000)->nullable();
+            $table->boolean('is_active')->default(0);
+            $table->boolean('is_visible_event_fe')->default(0);
+            $table->boolean('is_visible_event_bo')->default(0);
+            $table->boolean('is_closed_registration')->default(0);
+            $table->boolean('has_abstracts')->default(0);
+            $table->integer('abstract_limit')->default(3);
+            $table->boolean('is_closed_abstract_submission')->default(1);
+            $table->string('registration_sender_email', 255)->nullable();
+            $table->string('registration_sender_name', 255)->nullable();
+            $table->string('registration_email_subject', 255)->nullable();
+            $table->string('registration_reply_to_email', 255)->nullable();
+            $table->string('registration_cc_email', 255)->nullable();
+            $table->string('registration_bcc_email', 255)->nullable();
+            $table->string('abstract_email_subject', 255)->nullable();
+            $table->string('confirmation_email_subject', 255);
+            $table->boolean('has_confirmation_mail')->default(0);
+            $table->boolean('has_attachment_badge')->default(1);
+            $table->string('attachment_badge_name', 255);
+            $table->string('accent_color', 10)->default('#0091da');
+            $table->string('mailer', 20)->default('smtp');
             $table->timestamps();
         });
     }
